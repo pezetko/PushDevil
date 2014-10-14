f0087fb8edc83f00a33defd23a3abe03ffc89c42
Petr Zita <mgr.petr.zita@gmail.com>
2014-10-14 01:54:13 +0100

    Initial dummy commit.

---
+++ pushdevil/src/main/java/com/awensoftware/PushDevil.java
@@ -0,0 +1,13 @@
+package com.awensoftware;
+
+/**
+ * Hello world!
+ *
+ */
+public class PushDevil 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ pushdevil/src/test/java/com/awensoftware/PushDevilTest.java
@@ -0,0 +1,38 @@
+package com.awensoftware;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class PushDevilTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public PushDevilTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( PushDevilTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
